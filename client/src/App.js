718d96cd7edc142d4d90726cc16f606d80bf2b0e
Sreenath <sreenathes2001@gmail.com>
2023-02-12 22:14:45 +0530

    Designed course search result page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
+import CourseSearch from "./pages/student/CourseSearch";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route exact path="/" element={<Home />} />
+      <Route path="/course-search/:name" element={<CourseSearch />} />
     </Routes>
   );
 }
