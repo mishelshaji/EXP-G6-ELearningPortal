7f01730b80c055aed0bfeabb77d145d289163745
Bibinsasikumar21 <120707906+Bibinsasikumar21@users.noreply.github.com>
2023-02-13 10:22:49 +0530

    created login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
