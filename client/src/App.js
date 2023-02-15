75bce2e6fe205f3969f3b0e6f397d4764d9720e0
Nimishaer <120786011+Nimishaer@users.noreply.github.com>
2023-02-15 11:59:40 +0530

    Created a course request page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import CourseSearch from "./pages/student/CourseSearch";
+import CourseRequest from "./pages/admin/CourseRequest";
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route path="/search/:q" element={<CourseSearch />} />
+      <Route path="/CourseRequest" element={<CourseRequest/>}/>
     </Routes>
   );
 }
