2ed8124d1cef389f962a88d1ae7ab52f46ca8870
Varsha V <117258432+VARSHAV2001@users.noreply.github.com>
2023-02-15 15:52:02 +0530

    Update StudentProfile.js

--- client/src/pages/student/StudentProfile.js
+++ client/src/pages/student/StudentProfile.js
@@ -6,12 +6,9 @@ export default function StudentProfile() {
     fname: 'Sam',
     lname: 'Dio',
     email: 'sam@gmail.com',
-    age: 25,
-    gender: 'Male',
     mobile: 9988776655,
-    img: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=2000&q=60'
   };
-  
+
   return (
     <div>
       <div className='container mb-4'>
