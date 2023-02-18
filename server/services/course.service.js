4dff54e0df5fd4b3b92b0fc14a7eb8eefc08719c
Sreenath <sreenathes2001@gmail.com>
2023-02-18 13:15:49 +0530

    created fetch free course service

--- server/services/course.service.js
+++ server/services/course.service.js
@@ -29,7 +29,31 @@ const getAll = async () => {
 }
 
 const getAllFree = async () => {
+    const response = new ServiceResponse();
 
+    try {
+        const result = await Course.findAll({
+            where: { price: 0 }
+        });
+
+        const courses = result.map((course) => {
+            const dto = new CourseViewDTO();
+            dto.id = course.id;
+            dto.title = course.title;
+            dto.metaDescription = course.meta_description;
+            dto.level = course.level;
+            dto.price = course.price;
+            dto.featuredImageLink = course.featured_image_link;
+            dto.language = course.language;
+            dto.detailedDescription = course.detailed_description;
+            return dto;
+        });
+        response.result = { courses };
+        return response;
+    } catch (error) {
+        response.addError('Database', err);
+        return response;
+    }
 }
 
 const getOne = async () => {
