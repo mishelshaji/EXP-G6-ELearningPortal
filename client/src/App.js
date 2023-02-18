b2f9faa1f0b712d279ddd81fb3e2f01f7e866de5
Bibinsasikumar21 <120707906+Bibinsasikumar21@users.noreply.github.com>
2023-02-18 12:22:20 +0530

    created admin dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -5,50 +5,57 @@ import StudentLayout from './pages/student/StudentLayout';
 import AdminLayout from './pages/admin/AdminLayout';
 import UserLayout from './pages/UserLayout';
 import CourseSearch from './pages/student/CourseSearch';
-import Login from "./pages/Login";
+import Login from './pages/Login';
 import CourseListing from './pages/instructor/CourseList';
+import AdminDashboard from './pages/admin/AdminDashboard';
 
 const routes = createBrowserRouter([
-  {
-    path: '/',
-    element: <UserLayout />,
-    children: [
-      {
-        path: '',
-        element: <Home />
-      },
-      {
-        path: 'search/:q',
-        element: <CourseSearch />
-      },
-      {
-        path: '/login',
-        element: <Login />
-      }
-    ]
-  },
-  {
-    path: 'student',
-    element: <StudentLayout />
-  },
-  {
-    path: 'instructor',
-    element: <InstructorLayout />,
-    children: [
-      {
-        path: 'courses',
-        element: <CourseListing/>
-      }
-    ]
-  },
-  {
-    path: 'admin',
-    element: <AdminLayout />
-  }
+    {
+        path: '/',
+        element: <UserLayout />,
+        children: [
+            {
+                path: '',
+                element: <Home />
+            },
+            {
+                path: 'search/:q',
+                element: <CourseSearch />
+            },
+            {
+                path: '/login',
+                element: <Login />
+            }
+        ]
+    },
+    {
+        path: 'student',
+        element: <StudentLayout />
+    },
+    {
+        path: 'instructor',
+        element: <InstructorLayout />,
+        children: [
+            {
+                path: 'courses',
+                element: <CourseListing />
+            }
+        ]
+    },
+    {
+        path: 'admin',
+        element: <AdminLayout />,
+        children: [
+            {
+                path: '',
+                element: <AdminDashboard />
+            }
+        ]
+    }
 ]);
 
 function App() {
-  return <RouterProvider router={routes}></RouterProvider>;
+    return <RouterProvider router={routes}></RouterProvider>;
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/style.css';
 import './assets/login.css';
+import './assets/AdminDashboard.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
