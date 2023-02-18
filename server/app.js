3cf918fa502476176fed00e1eb62cad1f041fc8e
Sreenath <sreenathes2001@gmail.com>
2023-02-18 19:41:28 +0530

    updated course.service.js
    Created a function to generate DTO object.
    Added new function to fetch courses using userId.
    Added createdAt and updatedAt in course-view.dto.js

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 require('dotenv').config();
 const userRoutes = require('./modules/user/user.routes');
+const instructorRoutes = require('./modules/instructor/instructor.routes');
 
 const app = express();
 
 app.use(express.json());
 app.use('/', userRoutes);
+app.use('/instructor', instructorRoutes)
 
 app.listen(process.env.DEV_PORT);
---
+++ server/modules/instructor/controllers/course.controller.js
@@ -0,0 +1,37 @@
+const service = require('../../../services/course.service');
+
+const getAll = async (req, res) => {
+    const userId = req.params.id;
+    const result = await service.getCourseByUser(userId);
+    if (result.result) {
+        return res.status(200).json(result.result.courses);
+    } else if (!result.isValid) {
+        return res.status(400).json(result.errors);
+    } else {
+        return res.status(404).json(result.result);
+    }
+}
+
+const getOne = async (req, res) => {
+
+}
+
+const create = async (req, res) => {
+
+}
+
+const update = async (req, res) => {
+
+}
+
+const remove = async (req, res) => [
+
+]
+
+module.exports = {
+    getAll,
+    getOne,
+    create,
+    update,
+    remove
+}
