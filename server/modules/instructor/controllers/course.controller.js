9638680cfaeab8e753c828894e8006589a0e59c3
Sreenath <sreenathes2001@gmail.com>
2023-02-18 19:51:30 +0530

    Update course.controller.js
    Added function to fetch single course.

--- server/modules/instructor/controllers/course.controller.js
+++ server/modules/instructor/controllers/course.controller.js
@@ -13,7 +13,15 @@ const getAll = async (req, res) => {
 }
 
 const getOne = async (req, res) => {
-
+    const courseId = req.params.id;
+    const result = await service.getOne(courseId);
+    if (result.result) {
+        return res.status(200).json(result.result.course);
+    } else if (!result.isValid) {
+        return res.status(400).json(result.errors);
+    } else {
+        return res.status(404).json(result.result);
+    }
 }
 
 const create = async (req, res) => {
