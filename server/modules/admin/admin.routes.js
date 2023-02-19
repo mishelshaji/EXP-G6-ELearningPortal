90fcedec0eecce17c0be88559786f98c9278b2ba
Sreenath <sreenathes2001@gmail.com>
2023-02-19 22:23:08 +0530

    created course service functions

--- server/modules/admin/admin.routes.js
+++ server/modules/admin/admin.routes.js
@@ -3,7 +3,12 @@ const courseController = require('./controllers/course.controller');
 
 const router = express.Router();
 
+router.get('/courses/active', courseController.getAllActive);
+router.get('/courses/inactive', courseController.getAllInActive);
+router.get('/courses/deleted', courseController.getAllDeleted);
+router.get('/courses/:id', courseController.getOne);
 router.put('/courses/status/:id', courseController.updateStatus);
 router.put('/courses/price/:id', courseController.updatePrice);
+router.delete('/courses/:id', courseController.remove);
 
 module.exports = router;
