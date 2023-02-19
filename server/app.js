aee30cd015a79981fcc4ca0084c55662c9e3bbc2
Bibinsasikumar21 <120707906+Bibinsasikumar21@users.noreply.github.com>
2023-02-19 16:08:03 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,9 +5,13 @@ require('dotenv').config();
 const userRoutes = require('./modules/user/user.routes');
 const instructorRoutes = require('./modules/instructor/instructor.routes');
 const adminRoutes = require('./modules/admin/admin.routes');
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/', userRoutes);
+app.use('/instructor', instructorRoutes)
+app.use('/admin', adminRoutes)
 
 app.listen(process.env.DEV_PORT);
